c3549cc2e1da7b9e64ce5129c4b684a455f92f88
Patchipulusu Leela Krishna Raghavendra <patchipulusuraghavendra@gmail.com>
2026-05-15 20:22:37 +0530

    Update AWS Developer Associate certificate link

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { FaAws, FaGoogle, FaCertificate, FaExternalLinkAlt } from "react-icons/fa";
 
 const certs = [
-  { name: "AWS Developer Associate", icon: <FaAws />, link: "#" },
+  { name: "AWS Developer Associate", icon: <FaAws />, link: "https://drive.google.com/file/d/1wsZBHxZy_Vry1O9odeNXuOchrWr11Hf8/view" },
   { name: "AWS Cloud Developing", icon: <FaAws />, link: "#" },
   { name: "Google Cloud Career Launchpad", icon: <FaGoogle />, link: "#" },
   { name: "GFG Cloud Practitioner", icon: <FaCertificate />, link: "#" }
